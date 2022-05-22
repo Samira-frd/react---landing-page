f70dd6ab1f585d33dea1654a472e375c10c0f020
Samira Farokhnejad <samira.frd@hotmail.com>
2022-05-23 08:51:38 +1000

    Search bar is activated

--- src/component/Search.js
+++ src/component/Search.js
@@ -2,13 +2,37 @@ import React, { Component } from "react";
 import styles from "./Search.module.css";
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+    };
+  }
+
+  changeHandler = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+
   render() {
-    return(
-     <div className={styles.container}>
-         <p>Search what you are after</p>
-         <input placeholder="search"/>
-     </div>
-     );
+    return (
+      <div className={styles.container}>
+        <p>Search what you are after</p>
+
+        <div>
+          <input
+            type="text"
+            value={this.state.text}
+            onChange={this.changeHandler}
+            placeholder="search"
+          />
+          <br />
+          <br />
+          <span>{this.state.text}</span>
+        </div>
+      </div>
+    );
   }
 }
 
