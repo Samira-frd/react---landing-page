314c6e2f30f688a85a969fa05e4d640d2743b055
Samira Farokhnejad <samira.frd@hotmail.com>
2022-05-23 08:33:38 +1000

    Counter is added to the Card component section

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,19 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import styles from "./Card.module.css";
+import down from "../images/down.svg";
+import up from "../images/up.svg";
 
 class Card extends Component {
-    render() {
-        const {image} = this.props;
-        const {name} = this.props;
-        const {price} = this.props;
-        return (
-            <div className={styles.container}>
-                <img src={image} alt="Smart phone"/>
-                <h3>{name}</h3>
-                <p>{price}</p>
-            </div>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  downHandler = () => {
+      if( this.state.counter >= 1){
+          this.setState((prevState) => ({
+            counter: prevState.counter - 1,
+          }));
+      }
+  };
+
+  upHandler = () => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + 1,
+    }));
+  };
+
+  render() {
+    const { image } = this.props;
+    const { name } = this.props;
+    const { price } = this.props;
+    const { counter } = this.state;
+    return (
+      <div className={styles.container}>
+        <img src={image} alt="Smart phone" />
+        <h3>{name}</h3>
+        <p>{price}</p>
+        <div className={styles.counter}>
+          <img className={this.state.counter ? "": styles.deactive} src={down} alt="countDown" onClick={this.downHandler} />
+          <span>{counter}</span>
+          <img src={up} alt="countUp" onClick={this.upHandler} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Card;
